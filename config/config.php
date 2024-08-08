f72f7b0e5c9b23a1a9e6b9287da2bab719f879f4
Your Name <ise20016@stanley.co.id>
2024-08-08 16:38:19 +0700

    update env

--- config/config.php
+++ config/config.php
@@ -11,12 +11,9 @@ class Database{
             foreach ($env as $key => $value) {
                 $_ENV[$key] = $value;
             }
-            // $dsn = "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_DATABASE']}";
-            // $pdo = new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
             $this->conn = new PDO("mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_DATABASE']}", $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
             $this->conn->exec("set names utf8");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // echo "Database connection successful!";
         } catch (PDOException $e) {
             echo 'Connection failed: ' . $e->getMessage();
         }
