4446feb51ec1361da0ee382ec6c46e13b0b7fca2
Fadli Azka Prayogi <fadliazkaprayogi1@gmail.com>
2024-08-14 09:53:04 +0700

    added support csrf

---
+++ bin/support/Csrf.php
@@ -0,0 +1,26 @@
+<?php
+namespace Support;
+class CSRFToken {
+    public static function generateToken() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $token = bin2hex(random_bytes(32));
+        $_SESSION['csrf_token'] = $token;
+        return $token;
+    }
+
+    public static function validateToken($token) {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token)) {
+            unset($_SESSION['csrf_token']);
+            return true;
+        }
+        return false;
+    }
+}
+?>
