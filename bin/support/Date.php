2ee785f982f27ee694eed280479b4e24a38fedca
Fadli Azka Prayogi <fadliazkaprayogi1@gmail.com>
2024-08-19 10:34:42 +0700

    add helper date

---
+++ bin/support/Date.php
@@ -0,0 +1,55 @@
+<?php
+namespace Support;
+
+class Date{
+
+    protected $time;
+
+    public static function Now()
+    {
+        $date = date('Y-m-d H:i:s');
+        return $date;
+    }
+
+    public static function Day()
+    {
+        $date = date('d');
+        return $date;
+    } 
+
+    public static function Month()
+    {
+        $date = date('m');
+        return $date;
+    }
+
+    public static function Year()
+    {
+        $date = date('Y');
+        return $date;
+    }
+
+    public static function parse($parameter)
+    {
+        $time = is_numeric($parameter) ? $parameter : strtotime($parameter);
+        $instance = new self();
+        $instance->time = $time;
+        return $instance;
+    }
+
+    public function format($format)
+    {
+        return date($format,$this->time);
+    }
+
+    public function startOfDay()
+    {
+        return date('Y-m-d 00:00:00',$this->time);
+    }
+
+    public function endOfDay()
+    {
+        return date('Y-m-d 23:59:59',$this->time);
+    }
+}
+?>
