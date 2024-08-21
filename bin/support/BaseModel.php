2527d44606bad890db4ea656038f8b937355523f
Fadli Azka Prayogi <fadliazkaprayogi1@gmail.com>
2024-08-21 13:57:36 +0700

    update config

--- bin/support/BaseModel.php
+++ bin/support/BaseModel.php
@@ -30,7 +30,7 @@ class BaseModel
     private function connect()
     {
         try {
-            $dsn = "mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_DATABASE'];
+            $dsn = $_ENV['DB_CONNECTION'].":host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_DATABASE'];
             $this->connection = new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
