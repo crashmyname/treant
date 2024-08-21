748eb0ad24e5f87ba3d29e3aa3b53490abc5acf0
Fadli Azka Prayogi <fadliazkaprayogi1@gmail.com>
2024-08-21 11:58:52 +0700

    update config

--- config/config.php
+++ config/config.php
@@ -15,7 +15,7 @@ class Database
         // Nilai default untuk variabel lingkungan
         $defaultEnv = [
             'DB_HOST' => '127.0.0.1',
-            'DB_PORT' => '5432',
+            'DB_PORT' => '3306',
             'DB_DATABASE' => 'defaultdb', // Gunakan nama database default
             'DB_USERNAME' => 'root',
             'DB_PASSWORD' => '',
@@ -38,7 +38,7 @@ class Database
 
         try {
             // Buat koneksi database
-            $dsn = "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_DATABASE']}";
+            $dsn = "{$_ENV['DB_CONNECTION']}:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_DATABASE']}";
             $this->conn = new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
             $this->conn->exec("set names utf8");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
