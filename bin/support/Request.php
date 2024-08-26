0d5129f9990aeb291a14441bea4a6a28b1f5466d
Fadli Azka Prayogi <fadliazkaprayogi1@gmail.com>
2024-08-26 12:06:52 +0700

    update Request File

--- bin/support/Request.php
+++ bin/support/Request.php
@@ -3,9 +3,13 @@ namespace Support;
 
 class Request {
     private $data;
+    private $files;
 
     public function __construct() {
+        // Menggabungkan data GET dan POST
         $this->data = array_merge($this->sanitize($_GET), $this->sanitize($_POST));
+        // Menyimpan data FILES
+        $this->files = $this->sanitizeFiles($_FILES);
     }
 
     private function sanitize(array $data) {
@@ -15,6 +19,35 @@ class Request {
                 $sanitized[$key] = $this->sanitize($value); // Recursive sanitize for arrays
             } else {
                 $sanitized[$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+            }
+        }
+        return $sanitized;
+    }
+
+    private function sanitizeFiles(array $files) {
+        $sanitized = [];
+        foreach ($files as $key => $file) {
+            if (is_array($file['name'])) {
+                // Handle multiple files
+                $sanitized[$key] = [];
+                foreach ($file['name'] as $index => $name) {
+                    $sanitized[$key][] = [
+                        'name' => htmlspecialchars($name, ENT_QUOTES, 'UTF-8'),
+                        'type' => htmlspecialchars($file['type'][$index], ENT_QUOTES, 'UTF-8'),
+                        'tmp_name' => htmlspecialchars($file['tmp_name'][$index], ENT_QUOTES, 'UTF-8'),
+                        'error' => htmlspecialchars($file['error'][$index], ENT_QUOTES, 'UTF-8'),
+                        'size' => htmlspecialchars($file['size'][$index], ENT_QUOTES, 'UTF-8')
+                    ];
+                }
+            } else {
+                // Single file
+                $sanitized[$key] = [
+                    'name' => htmlspecialchars($file['name'], ENT_QUOTES, 'UTF-8'),
+                    'type' => htmlspecialchars($file['type'], ENT_QUOTES, 'UTF-8'),
+                    'tmp_name' => htmlspecialchars($file['tmp_name'], ENT_QUOTES, 'UTF-8'),
+                    'error' => htmlspecialchars($file['error'], ENT_QUOTES, 'UTF-8'),
+                    'size' => htmlspecialchars($file['size'], ENT_QUOTES, 'UTF-8')
+                ];
             }
         }
         return $sanitized;
@@ -31,6 +64,10 @@ class Request {
 
     public function all() {
         return $this->data;
+    }
+
+    public function file($key) {
+        return $this->files[$key] ?? null;
     }
 }
 ?>
